19eed4d39ed7b15348ea74492864d8765ce85539
marmooo <marmooo@users.noreply.github.com>
2022-09-11 08:54:56 +0900

    bump bootstrap from 5.2.0 to 5.2.1

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-08-28 08:51";
+var CACHE_NAME = "2022-09-11 08:49";
 var urlsToCache = [
   "/tegaki-yomi/",
   "/tegaki-yomi/index.js",
@@ -9,7 +9,7 @@ var urlsToCache = [
   "/tegaki-yomi/mp3/end.mp3",
   "/tegaki-yomi/eraser.svg",
   "/tegaki-yomi/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.0.7/dist/signature_pad.umd.min.js",
 ];
 
