ec5122df1d3ed6c5b547d4d187fa902236255f1c
marmooo <marmooo@users.noreply.github.com>
2022-10-15 12:59:16 +0900

    bump signature_pad from 4.0.9 to 4.0.10

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-10-09 10:18";
+var CACHE_NAME = "2022-10-15 12:58";
 var urlsToCache = [
   "/tegaki-yomi/",
   "/tegaki-yomi/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/tegaki-yomi/eraser.svg",
   "/tegaki-yomi/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.9/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.10/dist/signature_pad.umd.min.js",
 ];
 
 self.addEventListener("install", function (event) {
