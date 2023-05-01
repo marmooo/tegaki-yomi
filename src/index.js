6261cdfd0a002e1713e829b6c4c8bc19e6401b82
marmooo <marmooo@users.noreply.github.com>
2023-05-02 00:44:19 +0900

    cleanup

--- src/index.js
+++ src/index.js
@@ -350,11 +350,3 @@ document.addEventListener("click", unlockAudio, {
   once: true,
   useCapture: true,
 });
-
-// disable troublesome iOS features
-if (/Macintosh/.test(navigator.userAgent)) {
-  // double tap zoom
-  document.ondblclick = (e) => {
-    e.preventDefault();
-  };
-}
