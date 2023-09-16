a3e4df50efbfa0e69dcd917139d43bddf0f86114
marmooo <marmooo@users.noreply.github.com>
2023-09-16 16:38:08 +0900

    bump tfjs from 4.10.0 to 4.11.0

--- src/worker.js
+++ src/worker.js
@@ -39,7 +39,7 @@ async function loadModel() {
 }
 
 importScripts(
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.10.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.11.0/dist/tf.min.js",
 );
 
 let model;
