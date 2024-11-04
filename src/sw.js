3a5552d1258db0fdd89199c949b283e9d09269ac
marmooo <marmooo@users.noreply.github.com>
2024-11-04 13:29:44 +0900

    bump tfjs from 4.21.0 to 4.22.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2024-10-30 00:43";
+const CACHE_NAME = "2024-11-04 13:29";
 const urlsToCache = [
   "/tegaki-yomi/",
   "/tegaki-yomi/index.js",
@@ -8,7 +8,7 @@ const urlsToCache = [
   "/tegaki-yomi/mp3/correct3.mp3",
   "/tegaki-yomi/mp3/end.mp3",
   "/tegaki-yomi/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.21.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", (event) => {
--- src/worker.js
+++ src/worker.js
@@ -46,7 +46,7 @@ async function loadModelAndPredict(event) {
 }
 
 importScripts(
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.21.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js",
 );
 
 let model;
