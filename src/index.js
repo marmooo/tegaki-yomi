23de20b5d8f79ce50220d89d1ac0e07e09943ff3
marmooo <marmooo@users.noreply.github.com>
2025-09-20 11:19:12 +0900

    bump signature_pad from 5.1.0 to 5.1.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.0/+esm";
+import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.1/+esm";
 
 const playPanel = document.getElementById("playPanel");
 const infoPanel = document.getElementById("infoPanel");
